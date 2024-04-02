589ed809ca4fcfe33d66c81bc1042221902deac1
Roger Acosta <roger@mailvibes.io>
2024-04-01 18:19:29 -0600

    Users and Products Page design

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import Search from "@/app/ui/dashboard/search/search";
+import styles from "@/app/ui/dashboard/products/products.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const ProductsPage = () => {
   return (
-    <div>Products</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for products ..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.product}>
+                <Image
+                  src={"/noproduct.jpg"}
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.productImage}
+                />
+                IPhone
+              </div>
+            </td>
+            <td>john@gmail.com</td>
+            <td>13/01/2024</td>
+            <td>Admin</td>
+            <td>active</td>
+            <td>
+              <div className={styles.button}>
+                <Link href={"/"}>
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <Link href={"/"}>
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </Link>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;

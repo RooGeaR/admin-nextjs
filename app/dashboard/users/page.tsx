065e8711cf7efb176bd69f1a27502a776d6dc6cc
Roger Acosta <roger@mailvibes.io>
2024-04-05 17:41:16 -0600

    Added fetch users function and data for User Page

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,15 +1,17 @@
+import { fetchUsers } from '@/app/lib/data'
 import Pagination from '@/app/ui/dashboard/pagination/pagination'
 import Search from '@/app/ui/dashboard/search/search'
 import styles from '@/app/ui/dashboard/users/users.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const UsersPage = () => {
+const UsersPage = async () => {
+  const users = await fetchUsers();
   return (
     <div className={styles.container}>
       <div className={styles.top}>
-        <Search placeholder='Search for users ...'/>
-        <Link href='/dashboard/users/add'>
+        <Search placeholder="Search for users ..." />
+        <Link href="/dashboard/users/add">
           <button className={styles.addButton}>Add New</button>
         </Link>
       </div>
@@ -21,37 +23,49 @@ const UsersPage = () => {
             <td>Created At</td>
             <td>Role</td>
             <td>Status</td>
-            <td>Action</td>            
+            <td>Action</td>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <div className={styles.user}>
-                <Image src={"/noavatar.png"} alt='' width={40} height={40} className={styles.userImage}/>
-                John Doe
-              </div>
-            </td>
-            <td>john@gmail.com</td>
-            <td>13/01/2024</td>
-            <td>Admin</td>
-            <td>active</td>
-            <td>
-              <div className={styles.button}>
-                <Link href={`/dashboard/users/testid`}>
-                  <button className={`${styles.button} ${styles.view}`}>View</button>
-                </Link>
-                <Link href={"/"}>
-                  <button className={`${styles.button} ${styles.delete}`}>Delete</button>
-                </Link>
-              </div>
-            </td>
-          </tr>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    src={user.img ?? "/noavatar.png"}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                  />
+                  {user.username}
+                </div>
+              </td>
+              <td>{user.email}</td>
+              <td>{user.createdAt.toString().slice(4,16)}</td>
+              <td>{user.isAdmin ? "Admin" : "Client"}</td>
+              <td>{user.isActive ? "Active" : "Passive"}</td>
+              <td>
+                <div className={styles.button}>
+                  <Link href={`/dashboard/users/${user.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                  <Link href={"/"}>
+                    <button className={`${styles.button} ${styles.delete}`}>
+                      Delete
+                    </button>
+                  </Link>
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <Pagination />
     </div>
-  )
-}
+  );
+};
 
 export default UsersPage

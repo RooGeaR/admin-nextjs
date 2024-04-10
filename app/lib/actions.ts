e88e029e26d1c89f06d0880e94de4cc31501b69f
Roger Acosta <roger@mailvibes.io>
2024-04-10 17:43:56 -0600

    Added bcrypt for secure password

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,7 @@ import { revalidatePath } from "next/cache"
 import { User } from "./models"
 import { connectToDB } from "./utils"
 import { redirect } from "next/navigation"
+import bcrypt from 'bcrypt'
 
 export const addUser = async (formData: FormData) => {
   'use server'
@@ -9,8 +10,11 @@ export const addUser = async (formData: FormData) => {
   
   try {
     connectToDB()
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password as string, salt)
     const newUser = new User({
-      username, email, password, phone, address, isAdmin, isActive
+      username, email, password: hashedPassword, phone, address, isAdmin, isActive
     })
 
     await newUser.save()

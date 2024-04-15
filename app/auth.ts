298048969386f76e468ecd72b87fe5bbc56ec73d
Roger Acosta <roger@mailvibes.io>
2024-04-15 15:24:10 -0600

    Added next-auth library and functions to manage user session

---
+++ app/auth.ts
@@ -0,0 +1,49 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { authConfig } from "./auth.config";
+import { connectToDB } from "./lib/utils";
+import { User } from "./lib/models";
+import bcrypt from 'bcrypt'
+
+const login = async (credentials: Partial<Record<"username" | "password", unknown>>) => {
+  try {
+    connectToDB()
+    const user = await User.findOne({ username: credentials.username, isAdmin: true })
+
+    if (!user) {
+      throw new Error('Wrong credentials')
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(credentials.password as string, user.password)
+
+    if (!isPasswordCorrect) {
+      throw new Error('Wrong credentials')
+    }
+
+    return user
+  } catch (error) {
+    console.log(error)
+    throw new Error('Failed to login!')
+  }
+}
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        try {
+          const user = await login(credentials)
+          return user
+        } catch (error) {
+          return null
+        }
+      }
+    })
+  ]
+})
